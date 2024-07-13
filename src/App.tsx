2b55603992e8fca941a683ad2c633ed7abce8943
javadEsl <javades755@gmail.com>
2024-07-13 14:29:14 +0330

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import {MyComponent} from "./component";
+
+function App() {
+   return (
+    <div className={'app'}>
+      <MyComponent/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/component/MyComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MyComponent: React.FC = () => {
+    return <div>
+        <button>
+            Hello, this is my component!
+        </button>
+      </div>;
+};
+
